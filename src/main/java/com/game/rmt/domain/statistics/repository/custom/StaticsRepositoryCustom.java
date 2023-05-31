e4a24e901e3152c9ef79d64a423351b3aeacab94
jongyoon-park <cleanbook@naver.com>
2023-05-31 12:01:44 +0900

    feat: add getMonthlyGameStatics API

--- src/main/java/com/game/rmt/domain/statistics/repository/custom/StaticsRepositoryCustom.java
+++ src/main/java/com/game/rmt/domain/statistics/repository/custom/StaticsRepositoryCustom.java
@@ -2,11 +2,12 @@ package com.game.rmt.domain.statistics.repository.custom;
 
 import com.game.rmt.domain.statistics.dto.MonthlyStaticsDTO;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface StaticsRepositoryCustom {
-    List<MonthlyStaticsDTO> getMonthlyStaticsByGameId(Long gameId);
-    List<MonthlyStaticsDTO> getMonthlyStaticsByGameIdAndStartDate(Long gameId, String startDate);
-    List<MonthlyStaticsDTO> getMonthlyStaticsByGameIdAndEndDate(Long gameId, String endDate);
-    List<MonthlyStaticsDTO> getMonthlyStaticsByGameIdBetweenDate(Long gameId, String startDate, String endDate);
+    List<MonthlyStaticsDTO> getMonthlyStaticsByGameId(long gameId);
+    List<MonthlyStaticsDTO> getMonthlyStaticsByGameIdAndStartDate(long gameId, LocalDate startDate);
+    List<MonthlyStaticsDTO> getMonthlyStaticsByGameIdAndEndDate(long gameId, LocalDate endDate);
+    List<MonthlyStaticsDTO> getMonthlyStaticsByGameIdBetweenDate(long gameId, LocalDate startDate, LocalDate endDate);
 }
