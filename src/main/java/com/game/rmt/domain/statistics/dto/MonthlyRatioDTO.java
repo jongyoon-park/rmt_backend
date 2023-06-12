1bfc112ca8de6a82135218045e2719a17937065e
jongyoon-park <cleanbook@naver.com>
2023-06-12 18:16:16 +0900

    feat: add getMonthlyEachPlatformRatioByPreviousMonth API

--- src/main/java/com/game/rmt/domain/statistics/dto/MonthlyRatioDTO.java
+++ src/main/java/com/game/rmt/domain/statistics/dto/MonthlyRatioDTO.java
@@ -13,4 +13,20 @@ public class MonthlyRatioDTO {
     private final double PERCENTILE_RANK = 100.0;
 
     // 생성 시 증감 비율 계산해서 넣도록 변경하기
+    public MonthlyRatioDTO(String month, double currentPrice, double previousPrice) {
+        this.month = month;
+        this.percentage = calculatePercentage(currentPrice, previousPrice);
+    }
+
+    private double calculatePercentage(double currentPrice, double previousPrice) {
+        return roundTwoDecimalPlaces(calculateRatio(currentPrice, previousPrice));
+    }
+
+    private double roundTwoDecimalPlaces(double ratio) {
+        return Math.round(ratio * PERCENTILE_RANK) / PERCENTILE_RANK;
+    }
+
+    private double calculateRatio(double currentPrice, double previousPrice) {
+        return (currentPrice - previousPrice) / previousPrice * PERCENTILE_RANK;
+    }
 }
